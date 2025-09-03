07a1b114f5f7f53928267fb39eda2b99bd205eb8
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-03 20:03:24 +0100

    Update FollowBanner.swift

--- SuperScoreboard/Sources/Views/Components/FollowBanner.swift
+++ SuperScoreboard/Sources/Views/Components/FollowBanner.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct FollowBanner: View {
     var body: some View {
-        TrapezoidShape()
-            .foregroundStyle(Color(hex: "255AF6"))
-            .frame(height: 96)
+        background
+            .overlay(alignment: .leading) {
+                content
+            }
     }
 }
 
 #Preview {
     FollowBanner()
         .padding()
+}
+
+private extension FollowBanner {
+    var background: some View {
+        ZStack {
+            // Background trapezoid shape
+            TrapezoidShape()
+                .foregroundStyle(Color(hex: "255AF6"))
+                .frame(height: 96)
+
+            TrapezoidShape()
+                .foregroundStyle(.white.opacity(0.1))
+                .frame(height: 96)
+                .mask(alignment: .trailing, {
+                    Image("faceoff")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 170)
+                })
+        }
+    }
+
+    var content: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("Follow your favourites")
+                .font(.selecta(.medium, size: 16))
+
+            Text("Add your favourite athletes,\nteams & competitions to see the\nscores you care about")
+                .font(.selecta(.regular, size: 12))
+        }
+    }
 }
