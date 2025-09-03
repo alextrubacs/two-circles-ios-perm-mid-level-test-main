502c673e33b63651e5746394afdb6af6dc5b869b
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-03 11:04:18 +0100

    Add list

--- SuperScoreboard/Sources/ContentView.swift
+++ SuperScoreboard/Sources/ContentView.swift
@@ -5,10 +5,9 @@ struct ContentView: View {
     let viewModel = ContentViewModel()
 
     var body: some View {
-        VStack {
-            Text(String(describing: viewModel.matches))
+        List (viewModel.matches, id: \.id) { match in
+            Text(String(describing: match))
         }
-        .padding()
         .task {
             await viewModel.fetchMatches()
         }
