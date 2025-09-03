41fac0594fbeb149d58e72ea0d18f065f9619e77
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-03 18:20:34 +0100

    Update ContentView.swift

--- SuperScoreboard/Sources/ContentView.swift
+++ SuperScoreboard/Sources/ContentView.swift
@@ -7,7 +7,12 @@ struct ContentView: View {
     var body: some View {
         List (viewModel.matches, id: \.id) { match in
             ScoreCard(match: match)
+                .listRowSeparator(.hidden)
+                .listRowBackground(Color.clear)
+                .listRowSpacing(0)
+                .listRowInsets(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
         }
+        .listStyle(.insetGrouped)
         .task {
             await viewModel.fetchMatches()
         }
