df29f694438eb7f59b8ca3dbc302eda6924234e5
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-03 12:02:20 +0100

    Create ScoreCard.swift

---
+++ SuperScoreboard/Sources/Views/ScoreCard.swift
@@ -0,0 +1,28 @@
+//
+//  ScoreCard.swift
+//  SuperScoreboard
+//
+//  Created by Aleksandrs Trubacs on 03/09/2025.
+//
+
+import SwiftUI
+
+struct ScoreCard: View {
+    var body: some View {
+        HStack {
+            ClubBadge(
+                imageName: "Paris_Saint-Germain",
+                clubName: "PSG"
+            )
+
+            Text("2")
+                .font(.drukWide(.bold, size: 34))
+        }
+    }
+}
+
+#Preview {
+    ScoreCard()
+}
+
+
