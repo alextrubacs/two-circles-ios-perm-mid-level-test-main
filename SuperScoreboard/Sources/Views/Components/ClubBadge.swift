f7bf62cab896ed9e964302d19b39c86c6eb59bcd
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-03 12:02:23 +0100

    Create ClubBadge.swift

---
+++ SuperScoreboard/Sources/Views/Components/ClubBadge.swift
@@ -0,0 +1,30 @@
+//
+//  ClubBadge.swift
+//  SuperScoreboard
+//
+//  Created by Aleksandrs Trubacs on 03/09/2025.
+//
+
+import SwiftUI
+
+struct ClubBadge: View {
+    let imageName: String
+    let clubName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .renderingMode(.original)
+
+            Text(clubName)
+                .font(.selecta(.medium, style: .subheadline))
+        }
+    }
+}
+
+#Preview {
+    ClubBadge(
+        imageName: "Paris_Saint-Germain",
+        clubName: "PSG"
+    )
+}
