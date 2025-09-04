68155c2229902465a992e450746ddebab65797dc
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2025-09-04 12:55:35 +0100

    Update ItemViews.swift

--- SuperScoreboard/Sources/Views/Components/ItemViews.swift
+++ SuperScoreboard/Sources/Views/Components/ItemViews.swift
@@ -13,51 +13,85 @@ struct GenericItemView<Item: SectionItem>: View {
     @ViewBuilder
     private var itemImage: some View {
         if let teamItem = item as? TeamSectionItem {
-            ClubBadge(
-                imageName: teamItem.imageName,
-                clubName: teamItem.clubAbbr
-            )
+            teamItemView(for: teamItem)
         } else if let playerItem = item as? PlayerSectionItem {
-            Image(systemName: playerItem.imageName)
-                .font(.system(size: 40))
-                .foregroundColor(Color(hex: "255AF6"))
+            playerItemView(for: playerItem)
+        } else if let leagueItem = item as? LeagueSectionItem {
+            leagueItemView(for: leagueItem)
+        } else {
+            fallbackItemView
+        }
+    }
+}
+
+// MARK: - GenericItemView Extensions
+extension GenericItemView {
+
+    @ViewBuilder
+    private func teamItemView(for teamItem: TeamSectionItem) -> some View {
+        ClubBadge(
+            imageName: teamItem.imageName,
+            clubName: teamItem.clubAbbr
+        )
+    }
+
+    @ViewBuilder
+    private func playerItemView(for playerItem: PlayerSectionItem) -> some View {
+        Image(systemName: playerItem.imageName)
+            .font(.system(size: 40))
+            .foregroundColor(Color(hex: "255AF6"))
+            .overlay(alignment: .bottomTrailing) {
+                if isFavorited {
+                    FollowBadge()
+                }
+            }
+    }
+
+    @ViewBuilder
+    private func leagueItemView(for leagueItem: LeagueSectionItem) -> some View {
+        if leagueItem.imageName == "trophy.fill" {
+            leagueTrophyView(for: leagueItem)
+        } else {
+            leagueAssetView(for: leagueItem)
+        }
+    }
+
+    @ViewBuilder
+    private func leagueTrophyView(for leagueItem: LeagueSectionItem) -> some View {
+        Image(systemName: leagueItem.imageName)
+            .font(.system(size: 40))
+            .foregroundColor(.orange)
+            .overlay(alignment: .bottomTrailing) {
+                if isFavorited {
+                    FollowBadge()
+                }
+            }
+    }
+
+    @ViewBuilder
+    private func leagueAssetView(for leagueItem: LeagueSectionItem) -> some View {
+        VStack {
+            Image(leagueItem.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
                 .overlay(alignment: .bottomTrailing) {
                     if isFavorited {
                         FollowBadge()
                     }
                 }
-        } else if let leagueItem = item as? LeagueSectionItem {
-            if leagueItem.imageName == "trophy.fill" {
-                Image(systemName: leagueItem.imageName)
-                    .font(.system(size: 40))
-                    .foregroundColor(.orange)
-                    .overlay(alignment: .bottomTrailing) {
-                        if isFavorited {
-                            FollowBadge()
-                        }
-                    }
-            } else {
-                VStack {
-                    Image(leagueItem.imageName)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 40, height: 40)
-                        .overlay(alignment: .bottomTrailing) {
-                            if isFavorited {
-                                FollowBadge()
-                            }
-                        }
-                    Text(leagueItem.displayName)
-                        .font(.selecta(.medium, size: 16))
-                        .foregroundStyle(Color(hex: "1C1B19"))
-                        .textCase(nil)
-                }
-            }
-        } else {
-            Image(systemName: "questionmark.circle")
-                .font(.system(size: 40))
-                .foregroundColor(.gray)
+            Text(leagueItem.displayName)
+                .font(.selecta(.medium, size: 16))
+                .foregroundStyle(Color(hex: "1C1B19"))
+                .textCase(nil)
         }
+    }
+
+    @ViewBuilder
+    private var fallbackItemView: some View {
+        Image(systemName: "questionmark.circle")
+            .font(.system(size: 40))
+            .foregroundColor(.gray)
     }
 }
 
